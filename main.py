dc78ac5487f10c364d50c44a94103f2148bdc8e2
Srikant Panda <srikantap571@gmail.com>
2026-02-24 03:09:49 +0530

    update: reformatting the output of events.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import requests,json,os
+import requests,os,json
 import tabulate
 import argparse
 
-def github_activity(username):
+def github_activity(username='srikant-panda'):
 
     url = f'https://api.github.com/users/{username}/events'
 
@@ -27,39 +27,54 @@ if not data:
     print("Failed to fetch data. Exiting.")
     exit(1)
 
-def return_event(event,reponame):
-    if event.lower() == 'pushevent':
-        print(f"Pushed to {reponame}.")
-    elif event.lower() == 'pullrequestevent':
-        print(f'open a pull request from {reponame}.')
-    elif event.lower() == 'issuesevent':
-        print(f'opened a new issue in {reponame}.')
-    elif  event.lower() == 'deleteevent':
-        print(f'a brancg or tag deleted on {reponame}.')
-    elif event.lower() == 'issuecommentevent':
-        print(f'someone commented on an issue in {reponame}.')
-    elif event.lower() == 'pullrequestreviewevent':
-        print(f'someone submits a review on a pull request of {reponame}.')
-    elif event.lower() == 'watchevent':
-        print(f'cretaed a new repository  named {reponame}.')
-    elif event.lower() == 'forkevent':
-        print(f'{reponame} is forked.')
-    
+
+data = github_activity()
 
 
-# cli args
+activity = {}
 
-parser = argparse.ArgumentParser(description='Fetch GitHub user activity')
-parser.add_argument('username',type=str,help='Enter the username to get the events.')
+for i in data:
+    date = i['created_at'].split('T')[0]
+    reponame= i['repo']['name']
+    event = i['type']
+    if not date in activity:
+        activity[date] = {}
+    if not reponame in activity[date]:
+        activity[date][reponame] = {}
+    if not event in activity[date][reponame]:
+        activity[date][reponame][event] = 1
+    else:
+        activity[date][reponame][event] += 1
 
-args = parser.parse_args()
+with open('sample.json','w') as f:
+    json.dump(activity,f,indent=4)
 
 
-if __name__ == "__main__":
-    data = github_activity(args.username)
-    if data:
-        for event in data:
-            event_type = event['type']
-            repo_name = event['repo']['name']
-            return_event(event_type,repo_name)
+if __name__ == '__main__':
 
+    counter = 0
+    for date in activity:
+
+        print(date)
+
+        for repo in activity[date]:
+            print('     |')
+            print('      ->',repo)
+
+            for event in activity[date][repo]:
+                print()
+                print('                         ->',f'{event}({activity[date][repo][event]})')
+        counter +=1
+
+        if counter == 2:
+            choose = input('Enter (m) for more result or exit: ')
+            
+            if choose == 'm':
+                counter = 0
+                pass
+
+            elif choose == 'exit':
+                break
+        
+        
+       
